618e24fc94b3c61a6cbf6aa709cc631248e315e6
VincenzoDeLucia <vincenzo.delucia@icloud.com>
2020-11-25 18:05:13 +0100

    Master shanenigans

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,13 @@ const Request = require("../models/Request.model");
 
 router.get("/", (req, res, next) => {
   const { user } = req.session;
-  res.render("index", { user });
+  Book.find()
+    .sort({ dateAdded: -1 })
+    .limit(5)
+    .populate("owner")
+    .then((allTheBooks) => {
+      res.render("index", { user: user, books: allTheBooks });
+    });
 });
 
 router.get("/browse", (req, res) => {
